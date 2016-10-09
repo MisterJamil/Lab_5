af91f45e810fe6b8e22d15f15f5d75daca9cd962
Jamil <mr.jamilsaliba@gmail.com>
2016-10-09 16:57:44 -0700

    Initial commit
    Added decription to the Lab and also wrote out instructions for program.

--- Lab_5/Lab_5.cpp
+++ Lab_5/Lab_5.cpp
@@ -1,11 +1,30 @@
 // Lab_5.cpp : Defines the entry point for the console application.
-//
+// Author : Jamil Saliba
+// Date : 10/9/16
+// Description : Sores total rainfall of each month and returns total rainfall for the year, average monthly rainfall, and most/least rain month.
+
+/*Rainfall Program
+
+	Write a RainFall program that stores the total rainfall for each of 12 months into an array of doubles.
+
+	The program should have methods that return the following:
+
+*	The total rainfall for the year
+
+*	The average monthly rainfall
+
+*	The month number with the most rain (not the amount)
+
+*	The month number with the least rain (not the amount)
+
+	Demonstrate program. Input Validation should not allow negative numbers.
+*/
 
 #include "stdafx.h"
 
 
 int main()
-{
+{	int 
     return 0;
 }
 

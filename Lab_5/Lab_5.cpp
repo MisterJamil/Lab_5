4ed7b93e75471be77c226641a558ca8fc8dd5e46
Jamil <mr.jamilsaliba@gmail.com>
2016-10-09 19:37:05 -0700

    Changes and additions
    Added "&" sign to scanf, also added calculations for total rianfall and
    average rainfall.

--- Lab_5/Lab_5.cpp
+++ Lab_5/Lab_5.cpp
@@ -25,44 +25,55 @@
 
 int main()
 {
+	//Variable declaration
+
 	double dblJan, dblFeb, dblMar, dblApr, dblMay, dblJun, dblJul, dblAug, dblSep, dblOct, dblNov, dblDec, dblTotRain, dblAvgRain;
 	
+	//Input
 	printf("Type rainfall for month of January\n");
-	scanf("%lf", dblJan);
+	scanf("%lf", &dblJan);
 	
 	printf("Type rainfall for month of February\n");
-	scanf("%lf", dblFeb);
+	scanf("%lf", &dblFeb);
 	
 	printf("Type rainfall for month of March\n");
-	scanf("%lf", dblMar);
+	scanf("%lf", &dblMar);
 	
 	printf("Type rainfall for month of April\n");
-	scanf("%lf", dblApr);
+	scanf("%lf", &dblApr);
 	
 	printf("Type rainfall for month of May\n");
-	scanf("%lf", dblMay);
+	scanf("%lf", &dblMay);
 	
 	printf("Type rainfall for month of June\n");
-	scanf("%lf", dblJun);
+	scanf("%lf", &dblJun);
 	
 	printf("Type rainfall for month of July\n");
-	scanf("%lf", dblJul);
+	scanf("%lf", &dblJul);
 
 	printf("Type rainfall for month of August\n");
-	scanf("%lf", dblAug);
+	scanf("%lf", &dblAug);
 
 	printf("Type rainfall for month of September\n");
-	scanf("%lf", dblSep);
+	scanf("%lf", &dblSep);
 
 	printf("Type rainfall for month of October\n");
-	scanf("%lf", dblOct);
+	scanf("%lf", &dblOct);
 	
 	printf("Type rainfall for month of November\n");
-	scanf("%lf", dblNov);
+	scanf("%lf", &dblNov);
 	
 	printf("Type rainfall for month of December\n");
-	scanf("%lf", dblDec);
+	scanf("%lf", &dblDec);
 
+	//Calculations
+	dblTotRain = dblJan + dblFeb + dblMar + dblApr + dblMay + dblJun + dblJul + dblAug + dblSep + dblOct + dblNov + dblDec;
+	dblAvgRain = dblTotRain / 12;
+
+	//Output
+	printf("Total rainfall for the year : %.2lf", dblTotRain);
+	printf("Average monthly rainfall : %.2lf", dblAvgRain);
+	
 	return 0;
 }
 
